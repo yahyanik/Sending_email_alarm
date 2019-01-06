42c999c578016dfa3fe96ec18aa6db422b8b54a6
yahya <snikoue1@binghamton.edu>
2019-01-06 16:54:41 -0500

    web_page added

--- main_site.py
+++ main_site.py
@@ -6,12 +6,8 @@ from flask import Flask, render_template, Response
 if os.getuid() != 0:
     print("I can't run as a mortal. Sorry. Try to run me with admin privileges")
     exit()
-# import camera driver
 
-Camera = import_module('camera_opencv').Camera
-
-# Raspberry Pi camera module (requires picamera package)
-# from camera_pi import Camera
+Camera = import_module('camera_opencv_process').Camera
 
 app = Flask(__name__)
 
@@ -20,43 +16,17 @@ def index():
     """Video streaming home page."""
     return render_template('index.html')
 
-
-
-def genTxt(camera):
-    """Video streaming generator function."""
-    while True:
-        frame = camera.get_frame()[0]
-
-        # print frame
-        yield (str(frame))
-        # yield (b'--frame\r\n'b'Content-Type: text/xml; charset=utf-8\r\n\r\n' + str(frame[0]) + b'\r\n')
-
 def gen(camera):
     """Video streaming generator function."""
     while True:
         frame = camera.get_frame()
-        x = (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame[1] + b'\r\n')
-        # print frame
-        yield x
-        # yield (b'--frame\r\n'b'Content-Type: text/xml; charset=utf-8\r\n\r\n' + str(frame[0]) + b'\r\n')
-
+        yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/video_feed')
 def video_feed():
-
-    # print len(gen(Camera()))
     x = gen(Camera())
-    # print frame
-    # x = (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
     return Response(x,
                     mimetype='multipart/x-mixed-replace; boundary=frame')
-
-
-@app.route('/textdata')
-def textdata():
-    return  Response(genTxt(Camera()),
-                    mimetype='text/csv')
 
 
 
